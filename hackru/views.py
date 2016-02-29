a544548def46e6540a2068a34be3fb1ea66871b4
Sakib Jalal <sakib.jalal@gmail.com>
2016-02-29 02:13:38 -0500

    new static, templates, updated views, error handling, smoother error message flows

--- hackru/views.py
+++ hackru/views.py
@@ -16,219 +16,284 @@ def load_user(id):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/logout')
 @login_required
 def logout():
-    app.logger.info("{}[{}] at {} logged out".format(
-        current_user.name, current_user.id, request.remote_addr))
-    logout_user()
-    return redirect(url_for('index'))
+    try:
+        app.logger.info("{}[{}] at {} logged out".format(
+            current_user.name, current_user.id, request.remote_addr))
+        logout_user()
+        return redirect(url_for('index'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/dashboard', methods=['GET'])
 @login_required
 def dash():
-    if current_user.confirmed > 0:
-        if request.method == 'GET':
-            return render_template('dashboard.html',
-                                    name=current_user.name)
-    else:
-        return redirect(url_for('register'))
+    try:
+        if current_user.confirmed > 0:
+            if request.method == 'GET':
+                return render_template('dashboard.html',
+                                        name=current_user.name)
+        else:
+            return redirect(url_for('register'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/confirmstatus', methods=['GET'])
 @login_required
 def confirm_status():
-    if current_user.confirmed > 0:
-        if request.method == 'GET':
-            return render_template('change-confirming-status.html')
-    else:
-        return redirect(url_for('register'))
+    try:
+        if current_user.confirmed > 0:
+            if request.method == 'GET':
+                return render_template('change-confirming-status.html')
+        else:
+            return redirect(url_for('register'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/confirmed', methods=['GET'])
 @login_required
 def confirm():
-    if current_user.confirmed > 0:
-        if request.method == 'GET':
-            if current_user.confirmed != 2:
-                current_user.confirmed = 2
-                db.session.commit()
-                app.logger.info("{}[{}] at {} confirmed attendance".format(
-                    current_user.name, current_user.id, request.remote_addr))
-            return render_template('confirmed.html')
-    else:
-        return redirect(url_for('register'))
+    try:
+        if current_user.confirmed > 0:
+            if request.method == 'GET':
+                if current_user.confirmed != 2:
+                    current_user.confirmed = 2
+                    db.session.commit()
+                    app.logger.info("{}[{}] at {} confirmed attendance".format(
+                        current_user.name, current_user.id, request.remote_addr))
+                return render_template('confirmed.html')
+        else:
+            return redirect(url_for('register'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/notattending', methods=['GET'])
 @login_required
 def not_attending():
-    if current_user.confirmed > 0:
-        if request.method == 'GET':
-            if current_user.confirmed != 3:
-                current_user.confirmed = 3
-                db.session.commit()
-                app.logger.info("{}[{}] at {} confirmed lack of attendance".format(
-                    current_user.name, current_user.id, request.remote_addr))
-            return render_template('not-attending.html')
-    else:
-        return redirect(url_for('register'))
+    try:
+        if current_user.confirmed > 0:
+            if request.method == 'GET':
+                if current_user.confirmed != 3:
+                    current_user.confirmed = 3
+                    db.session.commit()
+                    app.logger.info("{}[{}] at {} confirmed lack of attendance".format(
+                        current_user.name, current_user.id, request.remote_addr))
+                return render_template('not-attending.html')
+        else:
+            return redirect(url_for('register'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 @login_required
 def register():
-
-    if current_user.confirmed == 0:
-        if request.method == 'GET':
-            github = current_user.github
-            resume = current_user.resume
-            comments = current_user.comments
-            if github is None: github = ""
-            if comments is None: comments = ""
-            if resume is None: resume = ""
-            return render_template('registration.html',
-                                    github=github,
-                                    resume=resume,
-                                    comments=comments)
-        if request.method == 'POST':
-
-            if not 'check' in request.form: # User must check MLH box
+    try:
+        if current_user.confirmed == 0:
+            if request.method == 'GET':
+                github = current_user.github
+                resume = current_user.resume
+                comments = current_user.comments
+                if github is None: github = ""
+                if comments is None: comments = ""
+                if resume is None: resume = ""
+                else: resume = resume[resume.index("_")+1:]
                 return render_template('registration.html',
-                        error="Please agree to the Code of Conduct.")
+                                        github=github,
+                                        resume=resume,
+                                        comments=comments)
+            if request.method == 'POST':
 
-	    if request.form:
-            	github = request.form.get('github')
-            	comments = request.form.get('comments')
+                if not 'check' in request.form: # User must check MLH box
+                    return render_template('registration.html',
+                            error="Please agree to the Code of Conduct.")
 
-	    """ for load testing
-	    elif request.json:
-		github = request.json.get('github')
-		comments = request.json.get('comments')
-	    """
+                # Upload file handling
+                file = request.files['resume']
+                if file:
+                    if allowed_file(file.filename):
+                        upload_file_handler(file)
+                    else:
+                        return render_template('registration.html',
+                            error="Please upload a PDF, TXT, DOC, or DOCX")
+                else:
+                    flash("Successfully updated account info!")
 
-            if github is None: github = ""
-            else: current_user.github = github
+                if request.form:
+                        github = request.form.get('github')
+                        comments = request.form.get('comments')
 
-            if comments is None: comments = ""
-            else: current_user.comments = comments
+                """ for load testing
+                elif request.json:
+                    github = request.json.get('github')
+                    comments = request.json.get('comments')
+                """
 
-            # Set user to registered
-            current_user.confirmed = 1
+                if github is None: github = ""
+                else: current_user.github = github
 
-            # Upload file handling
-            file = request.files['resume']
-            if file: flash(upload_file_handler(file))
-            else: flash("Successfully updated account info!")
+                if comments is None: comments = ""
+                else: current_user.comments = comments
 
-            db.session.commit()
+                # Set user to registered
+                current_user.confirmed = 1
 
-            app.logger.info("{}[{}] at {} registered successfully".format(
-                current_user.name, current_user.id, request.remote_addr))
+                db.session.commit()
 
-            return render_template('signup-good.html')
+                app.logger.info("{}[{}] at {} registered successfully".format(
+                    current_user.name, current_user.id, request.remote_addr))
 
-    else:
-        return redirect(url_for('dash'))
+                return render_template('signup-good.html')
+
+        else:
+            return redirect(url_for('dash'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/stats/<provider>', methods=['GET'])
 @login_required
 def stats(provider):
-    if current_user.is_admin:
-        oauth = OAuthSignIn.get_provider(provider)
-        users = oauth.get_users()
-        return render_template('stats.html', users=users)
-    else:
-        return redirect(url_for('index'))
+    try:
+        if current_user.is_admin:
+            oauth = OAuthSignIn.get_provider(provider)
+            users = oauth.get_users()
+            return render_template('stats.html', users=users)
+        else:
+            return redirect(url_for('index'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
 
-    if current_user.confirmed > 0:
-        if request.method == 'GET':
-            github = current_user.github
-            resume = current_user.resume
-            comments = current_user.comments
-            if github is None: github = ""
-            if comments is None: comments = ""
-            if resume is None: resume = ""
-            return render_template('manage.html',
-                                    github=github,
-                                    resume=resume,
-                                    comments=comments)
-        if request.method == 'POST':
+    try:
+        if current_user.confirmed > 0:
+            if request.method == 'GET':
+                github = current_user.github
+                resume = current_user.resume
+                comments = current_user.comments
 
-	    if request.form:
-            	github = request.form.get('github')
-            	comments = request.form.get('comments')
+                if github is None: github = ""
+                if comments is None: comments = ""
+                if resume is None: resume = ""
+                else: resume = resume[resume.index("_")+1:]
 
-	    """ For load testing
-	    elif request.json:
-		github = request.json.get('github')
-		comments = request.json.get('comments')
-	    """
+                return render_template('manage.html',
+                                        github=github,
+                                        resume=resume,
+                                        comments=comments)
+            if request.method == 'POST':
 
-            if github is None: github = ""
-            else: current_user.github = github
+                if request.form:
+                    github = request.form.get('github')
+                    comments = request.form.get('comments')
 
-            if comments is None: comments = ""
-            else: current_user.comments = comments
+                """ For load testing
+                elif request.json:
+                    github = request.json.get('github')
+                    comments = request.json.get('comments')
+                """
 
-            # Upload file handling
-            file = request.files['resume']
-            if file: flash(upload_file_handler(file))
-            else: flash("Successfully updated account info!")
+                # Upload file handling
+                file = request.files['resume']
+                if file:
+                    if allowed_file(file.filename):
+                        upload_file_handler(file)
+                    else:
+                        resume = current_user.resume
+                        if resume is None: resume = ""
+                        else: resume = resume[resume.index("_")+1:]
+                        return render_template('manage.html',
+                            github = github,
+                            comments = comments,
+                            resume = resume,
+                            error="Please upload a PDF, TXT, DOC, or DOCX")
+                else:
+                    flash("Successfully updated account info!")
 
-            db.session.commit()
+                if github is None: github = ""
+                else: current_user.github = github
 
-            app.logger.info("{}[{}] at {} updated account information".format(
-                current_user.name, current_user.id, request.remote_addr))
+                if comments is None: comments = ""
+                else: current_user.comments = comments
 
-            return redirect(url_for('dash'))
-    else:
-        return redirect(url_for('register'))
+
+                db.session.commit()
+
+                app.logger.info("{}[{}] at {} updated account information".format(
+                    current_user.name, current_user.id, request.remote_addr))
+
+                return redirect(url_for('dash'))
+        else:
+            return redirect(url_for('register'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('index'))
-    oauth = OAuthSignIn.get_provider(provider)
-    return oauth.authorize()
+    try:
+        if not current_user.is_anonymous:
+            return redirect(url_for('index'))
+        oauth = OAuthSignIn.get_provider(provider)
+        return oauth.authorize()
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
-    if not current_user.is_anonymous:
-        return redirect(url_for('index'))
-    oauth = OAuthSignIn.get_provider(provider)
-    mlh_id, name, email = oauth.callback()
-    if mlh_id is None:
-        flash('Authentication failed.')
-        return redirect(url_for('index'))
-    user = User.query.filter_by(mlh_id=mlh_id).first()
-    if not user:
-        # Create, add and login new user. Redirect to /register
-        user = User(mlh_id=mlh_id, name=name, email=email)
-        db.session.add(user)
-        db.session.commit()
-        login_user(user, True)
-        app.logger.info("{}[{}] at {} logged in for the first time".format(
-            current_user.name, current_user.id, request.remote_addr))
-        return redirect(url_for('register'))
-    else:
-        # Login new user. Redirect to /
-        login_user(user, True)
-        app.logger.info("{}[{}] at {} logged in".format(
-            current_user.name, current_user.id, request.remote_addr))
-        return redirect(url_for('index'))
+    try:
+        if not current_user.is_anonymous:
+            return redirect(url_for('index'))
+        oauth = OAuthSignIn.get_provider(provider)
+        mlh_id, name, email = oauth.callback()
+        if mlh_id is None:
+            flash('Authentication failed.')
+            return redirect(url_for('index'))
+        user = User.query.filter_by(mlh_id=mlh_id).first()
+        if not user:
+            # Create, add and login new user. Redirect to /register
+            user = User(mlh_id=mlh_id, name=name, email=email)
+            db.session.add(user)
+            db.session.commit()
+            login_user(user, True)
+            app.logger.info("{}[{}] at {} logged in for the first time".format(
+                current_user.name, current_user.id, request.remote_addr))
+            return redirect(url_for('register'))
+        else:
+            # Login new user. Redirect to /
+            login_user(user, True)
+            app.logger.info("{}[{}] at {} logged in".format(
+                current_user.name, current_user.id, request.remote_addr))
+            return redirect(url_for('index'))
+
+    except Exception as e:
+        return render_template('500.html', error=str(e))
 
 
 def allowed_file(filename):
@@ -237,26 +302,41 @@ def allowed_file(filename):
 
 
 def upload_file_handler(file):
-    if allowed_file(file.filename):
-        # Validate file size just in case
-        file.seek(0, os.SEEK_END)
-        if file.tell() > 2 * 1024 * 1024:
-            return "File too large! Maximum file size is 2 MB."
-        # Save file
-        filename = str(current_user.mlh_id) + "_" + secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        current_user.resume = filename
-        # Delete old resume
-        path = os.path.abspath(app.config['UPLOAD_FOLDER'])
-        list = os.listdir(path)
-        for item in list:
-            id = int(item.split('_')[0])
-            if id == int(current_user.mlh_id) and filename != item:
-                os.remove(os.path.join(path, item))
+    # Validate file size just in case
+    file.seek(0, os.SEEK_END)
+    if file.tell() > 2 * 1024 * 1024:
+        return "File too large! Maximum file size is 2 MB."
+    # Save file
+    filename = str(current_user.mlh_id) + "_" + secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    current_user.resume = filename
+    # Delete old resume
+    path = os.path.abspath(app.config['UPLOAD_FOLDER'])
+    list = os.listdir(path)
+    for item in list:
+        id = int(item.split('_')[0])
+        if id == int(current_user.mlh_id) and filename != item:
+            os.remove(os.path.join(path, item))
 
-        return "Successfully updated information!"
-    else:
-        return "Invalid file type! Please upload a PDF, TXT, DOC, DOCX"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(413)
+def file_too_large(e):
+    return render_template('413.html'), 413
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    return render_template('500.html'), 500
 
 
 """ Dummy route for load testing
